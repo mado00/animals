318b1a2f804eff44473c4c75ebcc874cd809a7d9
madoka <menomonies@gmail.com>
2023-02-09 18:43:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+  
+}
+// console.log(getRandomAnimal());
 
 function App() {
+  // count is state, setCount is setter function
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    // ...animals is creating new array of animals then
+    // pass in to the getRandomAnimal funtion
+    // CAN'T do that animals.push(getRandonAnimal())
+    // b/c cannot modify state "animals"
+    setAnimals([...animals, getRandomAnimal()])
+  };
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderAnimals}</div>
     </div>
   );
 }
